e566c48fdeaf90027660917bdb7249325d3f2b3c
VISHAL BALYAN <vbaliyan@researchilluminous.com>
2020-05-31 21:24:25 +0530

    Patch added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +29,50 @@ app.get('/todos', (req, res)=>{
 	}, (e)=>{
 		res.send(e);
 	});
+});
+
+app.get('/todos/:id',(req, res)=>{
+	var id = req.params.id;
+
+	if(!ObjectId.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e)=>{
+		res.status(400).send();
+	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(3000,()=>{
